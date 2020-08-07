5618966db5592795c767c1ff4233dd8e9ac9a9a4
Jackie Feit <jackiefeit94@gmail.com>
2020-08-07 15:35:53 -0400

    new repos with pwd, ls, cat

---
+++ bash.js
@@ -0,0 +1,11 @@
+const pwd = require('./pwd.js');
+const ls = require('./ls.js');
+const cat = require('./ls.js');
+
+process.stdout.write('prompt > ');
+process.stdin.on('data', (data) => {
+	const cmd = data.toString().trim();
+	pwd(cmd);
+	ls(cmd);
+	cat(cmd);
+});
---
+++ pwd.js
@@ -0,0 +1,7 @@
+const pwd = function (cmd) {
+	if (cmd.toLowerCase() === 'pwd') {
+		process.stdout.write(__dirname);
+		process.stdout.write('\nprompt > ');
+	}
+};
+module.exports = pwd;
